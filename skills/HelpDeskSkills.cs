780d31d5d8b6d29678d83706b975a5d9f0bdef1e
Sergey Ermolenko <sergey.ermolenko@telexy.com>
2023-10-24 13:06:01 -0700

    Latest HelpDeskSkills

--- skills/HelpDeskSkills.cs
+++ skills/HelpDeskSkills.cs
@@ -9,27 +9,47 @@ namespace Skills;
 /// </summary>
 public class HelpDeskSkill
 {
-    /// <summary>
-    /// GeQuoteAsync
+    //// <summary>
+    /// GeQpathAdditinalDepartmentQuoteAsync
     /// </summary>
-    /// <param name="quoteKind"></param>
     /// <param name="department"></param>
     /// <returns></returns>
-    [SKFunction, Description("Get Qpath System Deployment(Installation) Quote")]
-    public async Task<SKContext> GeQuoteAsync(
-        [Description("New Hospital or additional department installation")] string quoteKind,
-        [Description("Department Name (for department installation)")] string department)
+    [SKFunction, Description("Gets quote for adding department to existing Qpath Deployment.")]
+    public async Task<SKContext> GeQpathAdditionalDepartmentQuoteAsync([Description("Number of departments to add to Qpath")] int depNum)
     {
         await Task.CompletedTask;
-        return new SKContext();
+        var n = Guid.NewGuid();
+        var result = new SKContext(
+            new ContextVariables
+            ($"Quote for adding {depNum} departments to existing QPath Deployment created #: {n}. Please navigate to https://telexy.com/quotes/{n} to download the quote pdf file."));
+        return result;
     }
 
+    /// <summary>
+    /// GeQpathNewHospitalDeploymentQuoteAsync
+    /// </summary>
+    /// <param name="department"></param>
+    /// <returns></returns>
+    [SKFunction, Description("Get Quote for deploying new instance of Qpath in a new hospital.")]
+    public async Task<SKContext> GetQpathNewHospitalDeploymentQuoteAsync(
+        )
+    {
+        await Task.CompletedTask;
+        var n = Guid.NewGuid();
+        var result = new SKContext(
+            new ContextVariables
+            ($"Quote for deploying a new instance of QPath created #: {n}. Please navigate to https://telexy.com/quotes/{n} to download the quote pdf file."));
+        return result;
+    }
 
-    [SKFunction, Description("File ticket for Telexy Support Team")]
+    [SKFunction, Description("Creates and files ticket for the Telexy Support Team")]
     public async Task<SKContext> FileTicketAsync(
         [Description("Problem Description")] string description)
     {
         await Task.CompletedTask;
-        return new SKContext();
+        var n = Guid.NewGuid();
+        return new SKContext(
+            new ContextVariables(
+                $"Ticket created #: {n}. You can check the progress here https://telexy.com/tickets/{n}"));
     }
 }
